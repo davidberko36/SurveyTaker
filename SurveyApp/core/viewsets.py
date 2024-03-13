a48d38c69c827e45b2415a76ce992a16d0fd7046
davidberko36 <130989471+davidberko36@users.noreply.github.com>
2024-03-13 11:51:05 +0000

    Finished viewsets and serializers. Fine tuning to  follow

--- SurveyApp/core/viewsets.py
+++ SurveyApp/core/viewsets.py
@@ -6,3 +6,31 @@ from .serializers import UserSerializer, RegularUserSerializer, TeamSerializer, 
 class UserViewset(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class RegularUserViewset(viewsets.ModelViewSet):
+    queryset = RegularUser.objects.all()
+    serializer_class = RegularUserSerializer
+
+
+class TeamViewset(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class TeamLeaderViewset(viewsets.ModelViewSet):
+    queryset = TeamLeader.objects.all()
+    serializer_class = TeamLeaderSerializer
+
+
+class QuestionViewset(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class AnswerViewset(viewsets.ModelViewset):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+class SurveyViewset(viewsets.ModelViewSet):
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
